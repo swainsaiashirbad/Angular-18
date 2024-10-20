54d09767d04be29f508edca06404dfb33d6da628
saiAshirbad <swainsaiashirbad@gmail.com>
2024-10-20 15:11:27 +0530

    Reactive form added

---
+++ angular-18-tutorial/src/app/forms/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.css'
+})
+export class FormsComponent {
+
+}
---
+++ angular-18-tutorial/src/app/forms/reactive-form/reactive-form.component.ts
@@ -0,0 +1,35 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { variationPlacements } from '@popperjs/core';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [ReactiveFormsModule,JsonPipe],
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css'
+})
+export class ReactiveFormComponent {
+
+  formValue:any;
+
+  formObject:FormGroup=new FormGroup(
+    {
+      firstName:new FormControl("",[Validators.required,Validators.minLength(3)]),
+      lastName: new FormControl(""),
+      email: new FormControl("",[Validators.required,Validators.email]),
+      password: new FormControl(""),
+      city: new FormControl(""),
+      isAgreed: new FormControl(false,[Validators.required])
+
+    }
+  )
+
+
+
+
+  subbmit(){
+    this.formValue=this.formObject.value
+  }
+}
